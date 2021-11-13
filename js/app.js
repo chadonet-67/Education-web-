f681fcf48e4ae712db991cf1b81c774d732eba6d
chadonet-67 <chadonettzt@gmail.com>
2021-11-13 18:11:00 +0100

    fi du projet

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 let navbar = document.querySelector(".header .navbar");
 
-document.querySelector("#menu-btn").onclick = ()=>{
+document.querySelector('#menu-btn').onclick = ()=>{
     navbar.classList.toggle("active");
 }
 
 window.onscroll = ()=>{
     navbar.classList.remove("active");
+}
+
+let mainvid = document.querySelector(".main-video");
+
+document.querySelectorAll(".course-3 .box .video video").forEach(vid =>{
+    vid.onclick = () =>{
+        let src = vid.getAttribute('src');
+        mainvid.classList.add('active');
+        mainvid.querySelector('video').src = src;
+    }
+});
+
+document.querySelector("#close-vid").onclick = ()=>{
+    mainvid.classList.remove("active");
 }
